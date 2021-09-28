3ed9c0179dd46f02a32b276e7ee97cf8934e0c07
vilma-rojas <vilma_diana@outlook.com>
2021-09-28 06:51:55 -0300

    agrego clase repuesto

---
+++ Presentacion/Logica/Repuesto.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Logica
+{
+    class Repuesto
+    {
+        private String nombre;
+
+        private float precio;
+
+        public Repuesto(String nombre, float precio)
+        {
+            this.nombre = nombre;
+            this.precio = precio;
+
+        }
+
+        public String getNombre()
+        {
+            return this.nombre;
+        }
+
+        public float getPrecio()
+        {
+            return this.precio;
+        }
+
+        public void setNombre(String nombre)
+        {
+            this.nombre = nombre;
+        }
+
+        public void setPrecio(float precio)
+        {
+            this.precio = precio;
+        }
+    }
+}
