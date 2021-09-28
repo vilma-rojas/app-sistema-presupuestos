e2faaa6482ee544110d789e21f60eda21afa7a44
vilma-rojas <vilma_diana@outlook.com>
2021-09-28 06:48:03 -0300

    agrego clase vehiculo

--- Presentacion/Logica/Class1.cs
+++
@@ -1,8 +0,0 @@
-using System;
-
-namespace Logica
-{
-    public class Class1
-    {
-    }
-}
---
+++ Presentacion/Logica/Vehiculo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Logica
+{
+    class Vehiculo
+    {
+        private String marca;
+
+        private String modelo;
+
+        private int patente;
+    }
+}
