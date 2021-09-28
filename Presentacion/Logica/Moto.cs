df05ca69613e7683214deedc63228c5e89a52068
vilma-rojas <vilma_diana@outlook.com>
2021-09-28 06:50:15 -0300

    agrego clase moto

---
+++ Presentacion/Logica/Moto.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Logica
+{
+    class Moto
+    {
+        private bool cilindrada;
+
+        public Moto()
+        {
+            cilindrada = true;
+        }
+
+        public Moto(bool cilindrada)
+        {
+            this.cilindrada = cilindrada;
+        }
+
+        public void getCilindrada(bool cilindrada)
+        {
+            this.cilindrada = cilindrada;
+        }
+
+        public bool setCilindrada()
+        {
+            return this.cilindrada;
+        }
+    }
+}
