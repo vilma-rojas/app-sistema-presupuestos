0d631e48b914493747fa69fc009bccbb3ac98942
vilma-rojas <vilma_diana@outlook.com>
2021-09-28 06:49:21 -0300

    agrego clase automovil

---
+++ Presentacion/Logica/Automovil.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Logica
+{
+    class Automovil
+    {
+        private String tipo;
+
+        private int cantPuertas;
+
+        public Automovil(String tipo)
+        {
+            this.tipo = tipo;
+            this.cantPuertas = 4;
+        }
+
+        public Automovil(String tipo, int cantPuertas)
+        {
+            this.tipo = tipo;
+            this.cantPuertas = cantPuertas;
+        }
+
+        public String getTipo()
+        {
+            return this.tipo;
+        }
+
+        public int getCantPuertas()
+        {
+            return this.cantPuertas;
+        }
+
+        public void setTipo(String tipo)
+        {
+            this.tipo = tipo;
+        }
+
+        public void setCantPuertas(int cantPuertas)
+        {
+            this.cantPuertas = cantPuertas;
+        }
+    }
+}
